ea71d08373dde762021beb6d2160bbf74f7be15b
AmosZhang <moonyellow@126.com>
2024-04-20 12:58:32 +0800

    Deploy website - based on d6944d3342e0dad747a6cc662dadad97c58d9fe7

---
+++ js/custom.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // 获取 URL 中的锚点
+  let hash = window.location.hash;
+  console.log("hash", hash);
+  // 如果存在锚点，则滚动到该位置
+  if (hash) {
+    let targetElement = document.querySelector(hash);
+    console.log("targetElement", targetElement);
+    // holy-grail-layout
+    if (targetElement) {
+      targetElement.scrollIntoView();
+    }
+  }
+});
+console.log('custom static')
